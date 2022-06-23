39897db6ede69d568fdca1413257aa8fdacc88c9
Sam Feng <fengyuan20000303@gmail.com>
2022-06-23 22:45:15 +0800

    WIP State Machine

--- systems/statemachine/player_states/walk_state.gd
+++ systems/statemachine/player_states/walk_state.gd
@@ -11,6 +11,7 @@ var direction = Vector2.ZERO
 
 var controlling = false
 
+
 func get_class(): 
 	return "WalkState"
 
@@ -18,16 +19,18 @@ func get_class():
 func _ready():
 	pass
 
+
 func check_state_change():
 	if controlling:
 		persistent_state.velocity = Vector2.ZERO
 		change_state.call_func("control")
 		
+	# change to idle state if velocity reach zero
 	elif abs(persistent_state.velocity.length()) < min_move_speed:
 		change_state.call_func("idle")
 
+
 func _physics_process(_delta):
-	# change to idle state if velocity reach zero
 	
 	# else deal with the velocity
 	if direction.length() > 0:
@@ -36,6 +39,8 @@ func _physics_process(_delta):
 		persistent_state.velocity = lerp(persistent_state.velocity, Vector2.ZERO, friction)
 	
 	direction = Vector2.ZERO
+	persistent_state.move_and_slide(persistent_state.velocity)
+	
 	check_state_change()
 
 
