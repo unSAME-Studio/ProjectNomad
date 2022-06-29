88657e474d4b8283d7d3cfdb8656637890bb2f61
Sam Feng <fengyuan20000303@gmail.com>
2022-06-29 23:32:30 +0800

    WIP Art

--- objects/controllable/culpit.gd
+++ objects/controllable/culpit.gd
@@ -16,9 +16,13 @@ func get_hint_text():
 
 
 func initial_control(body):
+	# snap body to predefined point
 	body.set_global_position($ControlPos.get_global_position())
-	base.enable_control(body)
-	body.camera.camera.set_zoom(Vector2(2,2))
+	
+	if base.has_method("enable_control"):
+		base.enable_control(body)
+	
+	#body.camera.camera.set_zoom(Vector2(2,2))
 	
 	print(name + " is being controller")
 
@@ -26,5 +30,7 @@ func initial_control(body):
 func stop_control(body):
 	print("stopping " + name + " from controlling")
 	
-	base.disable_control()
-	body.camera.camera.set_zoom(Vector2(1,1))
+	if base.has_method("disable_control"):
+		base.disable_control()
+	
+	#body.camera.camera.set_zoom(Vector2(1,1))
