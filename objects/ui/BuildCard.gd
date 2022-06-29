a23d3b8543b41478c5feae34d67f105c2ed006b3
Sam Feng <fengyuan20000303@gmail.com>
2022-06-29 22:54:28 +0800

    Building Card WIP

--- objects/ui/BuildCard.gd
+++ objects/ui/BuildCard.gd
@@ -1,39 +1,14 @@
 extends TextureButton
 
-enum TYPE {WHEEL, TURRET, AUTO_TURRET, LOCKER}
-export(TYPE) var build_type = TYPE.WHEEL
+export(String) var build_type = "wheel"
 
-var cards_group = preload("res://objects/ui/BuildCardsGroup.tres")
-
-var last_state = pressed
-
-
-var icons = {
-	TYPE.WHEEL: "res://arts/cards/C_wheel.png",
-	TYPE.TURRET: "res://arts/cards/C_turret.png",
-	TYPE.AUTO_TURRET: "res://arts/cards/C_auto_turret.png",
-	TYPE.LOCKER: "res://arts/cards/C_locker.png",
-}
+var prebuild = preload("res://objects/buildings/Prebuild.tscn")
 
 
 func _ready():
-	set_normal_texture(load(icons[build_type]))
-	set_pressed_texture(load(icons[build_type]))
-	set_hover_texture(load(icons[build_type]))
-	
-	# blong in one group
-	set_button_group(cards_group)
-
-
-func _process(delta):
-	if pressed != last_state:
-		last_state = pressed
-		
-		# set modulate accordingly
-		if pressed:
-			set_modulate(Color("c39f20"))
-		else:
-			set_modulate(Color.white)
+	set_normal_texture(load(Global.culpits_data[build_type]["card"]))
+	set_pressed_texture(load(Global.culpits_data[build_type]["card"]))
+	set_hover_texture(load(Global.culpits_data[build_type]["card"]))
 
 
 func _on_BuildCard_mouse_entered():
@@ -44,3 +19,14 @@ func _on_BuildCard_mouse_entered():
 func _on_BuildCard_mouse_exited():
 	set_scale(Vector2(1, 1))
 	set_position(Vector2(get_position().x, 0))
+
+
+func _on_BuildCard_pressed():
+	set_disabled(true)
+	set_modulate(Color("7d737373"))
+	
+	var p = prebuild.instance()
+	p.card = self
+	p.type = build_type
+	Global.player.get_parent().add_child(p)
+	
