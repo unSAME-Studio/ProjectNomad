d9fb8d3220b2af05f940c8b0209ab9ca10152fef
EiZi233 <57603782+EiZi233@users.noreply.github.com>
2022-07-01 04:09:26 -0700

    Build point prototype

--- objects/rooms/room.gd
+++ objects/rooms/room.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var base = null
-
+var build_points = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,12 +9,22 @@ func _ready():
 	$structures/Wall/LightOccluder2D.occluder.set_polygon($structures/Wall/Polygon2D.polygon)
 	$CollisionPolygon2D.polygon = $Polygon2D.polygon
 	base = get_parent().get_parent()
-	
+	base.rooms.append(self)
+	build_points = $structures/points.get_children()
+	for i in build_points:
+		i.room = self
+
 	#pass
 #will return the base
 func get_build():
 	return base
-
+	
+func get_build_points(type):
+	var out_points = []
+	for i in $structures/points.get_children():
+		if i.type == type:
+			out_points.append(i)
+	return out_points
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
