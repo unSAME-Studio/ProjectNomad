c1fe3b55b284acaf2a9f097bc2e58de7090ee692
EiZi233 <57603782+EiZi233@users.noreply.github.com>
2022-07-01 11:20:24 -0700

    fix

--- objects/rooms/SetPolygonCollider.gd
+++ objects/rooms/SetPolygonCollider.gd
@@ -8,7 +8,8 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CollisionPolygon2D.polygon = $Polygon2D.polygon
+	$LightOccluder2D.occluder.set_polygon($Polygon2D.polygon)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
