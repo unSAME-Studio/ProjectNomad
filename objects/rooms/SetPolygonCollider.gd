4e560cee3951bbdca22612418c4beaf71f6ab0d6
Sam Feng <fengyuan20000303@gmail.com>
2022-07-02 11:27:12 +0800

    Cleaning

--- objects/rooms/SetPolygonCollider.gd
+++ objects/rooms/SetPolygonCollider.gd
@@ -1,17 +1,9 @@
 extends StaticBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$CollisionPolygon2D.polygon = $Polygon2D.polygon
 	$LightOccluder2D.occluder.set_polygon($Polygon2D.polygon)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
