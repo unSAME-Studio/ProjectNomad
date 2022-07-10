96aaeddbc982fe68cffa47faed91322ade49eb35
Sam Feng <fengyuan20000303@gmail.com>
2022-07-11 01:30:18 +0800

    Pretty Trees

--- scripts/parallax_offset.gd
+++ scripts/parallax_offset.gd
@@ -1,11 +1,13 @@
 extends Polygon2D
 
-onready var initial_position = get_node("../Polygon2D").get_position()
+onready var initial_position = get_node("../Polygon2D").get_global_position()
+
+export var offset_amount = 1.0
 
 
 func _process(delta):
 	var position_offset = get_global_position() - Global.player.camera.camera.get_camera_position()
-	position_offset = (position_offset / 10) * -1
+	position_offset = (position_offset / 10) * offset_amount
 	
 	#print(initial_position + position_offset)
-	set_position(initial_position + position_offset)
+	set_global_position(initial_position + position_offset)
--- scripts/parallax_offset_sprite.gd
+++ scripts/parallax_offset_sprite.gd
@@ -1,13 +1,13 @@
 extends Sprite
 
-onready var initial_position = get_node("../Root").get_position()
+onready var initial_position = get_global_position()
 
 export var offset_amount = 1.0
 
 
 func _process(delta):
 	var position_offset = get_global_position() - Global.player.camera.camera.get_camera_position()
-	position_offset = (position_offset / 10) * -1 * offset_amount
+	position_offset = (position_offset / 10) * offset_amount
 	
 	#print(initial_position + position_offset)
-	set_position(initial_position + position_offset)
+	set_global_position(initial_position + position_offset)
