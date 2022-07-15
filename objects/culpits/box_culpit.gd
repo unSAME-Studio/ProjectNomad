bc354ce86f6c8d984d9564975c61da950fc3d77d
Sam Feng <fengyuan20000303@gmail.com>
2022-07-15 20:01:54 +0800

    Create box_culpit.gd

---
+++ objects/culpits/box_culpit.gd
@@ -0,0 +1,32 @@
+extends Culpit
+
+var storing = null
+var count = 0
+
+
+func _ready():
+	._ready()
+	
+	var area2d = Area2D.new()
+	add_child(area2d)
+	
+	var collision = CollisionShape2D.new()
+	var circle = CircleShape2D.new()
+	collision.set_shape(circle.set_radius(RANGE))
+	area2d.add_child(collision)
+
+
+func _process(delta):
+	pass
+
+
+func operate(player):
+	pass
+
+
+func initial_control(body):
+	print("HOLDING A BOX")
+
+
+func stop_control(body):
+	print("stopping " + type + " from controlling")
