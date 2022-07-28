1d9f1d24969fb510000e6ea5ff775a009afd55e9
Sam Feng <fengyuan20000303@gmail.com>
2022-07-28 23:57:24 +0800

    Cooldown Component Added

--- objects/culpits/turret_culpit.gd
+++ objects/culpits/turret_culpit.gd
@@ -5,12 +5,20 @@ var controlling = false
 
 var bullet = preload("res://objects/weapons/bullet.tscn")
 
+var cooldown_comp = preload("res://systems/CooldownComponent.tscn")
+var cooldown
+
 
 func _ready():
 	var p = Position2D.new()
 	$Sprite.add_child(p)
 	p.set_name("Position2D")
 	p.set_position(Vector2(10, 0))
+	
+	cooldown = cooldown_comp.instance()
+	add_child(cooldown)
+	cooldown.set_name("CooldownComponent")
+	cooldown.speed = 10
 
 
 func _process(delta):
@@ -19,12 +27,15 @@ func _process(delta):
 
 
 func operate(player):
-	print("FIRE!!!")
-	var b = bullet.instance()
-	b.parent = self
-	b.set_global_position($Sprite/Position2D.get_global_position())
-	b.set_global_rotation($Sprite.get_global_rotation())
-	get_tree().get_current_scene().get_node("Node2D").add_child(b)
+	if cooldown.can_fire(10):
+		print("FIRE!!!")
+		var b = bullet.instance()
+		b.parent = self
+		b.set_global_position($Sprite/Position2D.get_global_position())
+		b.set_global_rotation($Sprite.get_global_rotation())
+		get_tree().get_current_scene().get_node("Node2D").add_child(b)
+		
+		cooldown.increase_cooldown(10)
 
 
 func initial_control(body):
