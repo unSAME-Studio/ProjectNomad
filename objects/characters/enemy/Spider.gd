c660ab3599192107c313f3b93f33debdfe252673
Sam Feng <fengyuan20000303@gmail.com>
2022-08-13 00:58:22 +0800

    Spider Added

--- objects/characters/enemy/Spider.gd
+++ objects/characters/enemy/Spider.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 var bullet = preload("res://objects/weapons/bullet.tscn")
 var speed = 100
 var user = self
+var cd = 20
 
 export var immobile = true
+
 
 func _ready():
 	pass
@@ -29,15 +31,21 @@ func _physics_process(delta):
 
 
 func _on_Timer_timeout():
-	if Global.player.get_global_position().distance_to(get_global_position()) <= 1000:
-		if $RayCast2D.is_colliding():
-			$AnimationPlayer.play("shoot")
-			
-			var b = bullet.instance()
-			b.parent = self
-			b.set_global_position($Enemy/Position2D.get_global_position())
-			b.set_global_rotation($Enemy/Position2D.get_global_rotation())
-			get_tree().get_current_scene().get_node("Node2D").add_child(b)
+	if $CooldownComponent.can_fire(cd):
+		if Global.player.get_global_position().distance_to(get_global_position()) <= 1000:
+			if $Enemy/RayCast2D.is_colliding():
+				
+				#print($Enemy/RayCast2D.get_collider().name)
+				if "Player" == $Enemy/RayCast2D.get_collider().name:
+					$AnimationPlayer.play("shoot")
+					
+					var b = bullet.instance()
+					b.parent = self
+					b.set_global_position($Enemy/Position2D.get_global_position())
+					b.set_global_rotation($Enemy/Position2D.get_global_rotation())
+					get_tree().get_current_scene().get_node("Node2D").add_child(b)
+					
+					$CooldownComponent.increase_cooldown(cd)
 
 
 func _on_VisibilityEnabler2D_screen_entered():
