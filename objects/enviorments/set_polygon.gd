1071ecbc008f048cfc16e017ac1aae8a85bc97f7
Sam Feng <fengyuan20000303@gmail.com>
2022-09-06 22:19:56 +0800

    Added Rock Polygon Funb

--- objects/enviorments/set_polygon.gd
+++ objects/enviorments/set_polygon.gd
@@ -3,3 +3,6 @@ extends StaticBody2D
 
 func _ready():
 	$CollisionPolygon2D.polygon = $Polygon2D.polygon
+	$LightOccluder2D.occluder.polygon = $Polygon2D.polygon
+	$CollisionPolygon2D.set_global_transform($Polygon2D.get_global_transform())
+	$LightOccluder2D.set_global_transform($Polygon2D.get_global_transform())
