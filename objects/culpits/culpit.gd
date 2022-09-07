7f09ee73a9dd4ddd87ae0ce2b08382092985e7c5
Sam Feng <fengyuan20000303@gmail.com>
2022-09-07 14:24:15 +0800

    Update culpit.gd

--- objects/culpits/culpit.gd
+++ objects/culpits/culpit.gd
@@ -41,7 +41,7 @@ func _ready():
 	connect("mouse_entered", self, "_on_mouse_entered")
 	connect("mouse_exited", self, "_on_mouse_exited")
 	
-	if Global.player:
+	if Global.player and is_instance_valid(Global.player):
 		Global.player.play_sound("Build", get_global_position())
 	
 	# don't allow player to control if not controllable
